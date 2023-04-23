59344d8c31d9b3f6e4d45330ec98279cb73b2b25
Yitzhakpro <isako14@gmail.com>
2023-04-23 17:06:21 +0300

    fixed type of chat-backend

--- apps/agency-chat-backend/jest.config.ts
+++ apps/agency-chat-backend/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'agegncy-chat-backend',
+  displayName: 'agency-chat-backend',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/agegncy-chat-backend',
+  coverageDirectory: '../../coverage/apps/agency-chat-backend',
 };
